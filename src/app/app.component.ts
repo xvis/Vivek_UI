a5ac93f56f4a8064b4eb108b34b4d4c6e961fd4b
Vivek <vic786266@gmail.com>
2025-01-25 08:14:22 +0530

    first push

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,5 +9,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'Angular-Demo';
+  title = 'First time push code to Github';
 }
